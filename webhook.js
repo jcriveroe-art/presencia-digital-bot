1963a79ca60bbeafc5d8094517a7d8cff5c32a71
jcriveroe-art <jcriveroe@gmail.com>
2026-06-24 19:15:14 -0600

    fix: mover telegram secrets a env vars

--- webhook.js
+++ webhook.js
@@ -195,8 +195,8 @@ async function sendMessage(to, message) {
 async function enviarTelegram(mensaje) {
   try {
     const axios = require("axios");
-    const token = "8860116821:AAFr4OFeFG6G0CRLsPhJdXvmAzWxHS7AG2A";
-    const chatId = "7064740340";
+    const token = process.env.TELEGRAM_BOT_TOKEN;
+    const chatId = process.env.TELEGRAM_CHAT_ID;
     await axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
       chat_id: chatId,
       text: mensaje,
